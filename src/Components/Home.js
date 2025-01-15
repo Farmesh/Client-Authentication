0050dd56c20f909319db677c7dbf7dfc3db9a0e9
Farmesh <thakurfarmesh123@gmail.com>
2025-01-15 10:02:23 +0530

    comment added dummy

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 
 function Home() {
+  //added testing comment 
+  //git test 
   return (
     <div style={{ backgroundColor: '#f0f8ff', padding: '20px', borderRadius: '10px' }}>
       <h1 className="text-center m-4 text-primary"> Hello {localStorage.user}</h1>
